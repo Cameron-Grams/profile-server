0555c7c2dee0264136a3af29fdf190800c9d335a
Cameron <cameron.grams.2@gmail.com>
2018-05-13 13:52:52 -0400

    work on the project objects

--- projectFile.js
+++ projectFile.js
@@ -1,27 +1,27 @@
 const projects = {
     1: {
         title: "Analyst Timeline",
-        firstParagraph: "First text",
-        secondParagraph: "Second text",
-        thirdParagraph: "Third text"
+        firstParagraph: "This project was built using React, Redux, and MongoDB. It relies on hosting by Heroku for the server endpoints and mLab for the database hosting. There are separate data collections for users, timelines and timeline entries.",
+        secondParagraph: "I find a graphic reprsentation of information helpful, especially when trying to events in context. This is the tool I wish that I had had before my second deployment to Iraq when I was trying to understand Iraqi foreign relations. ",
+        thirdParagraph: "The next step will be to migrate the timeline from a CSS component to using SVG."
        },
     2: {
         title: "532 Packard Street",
-        firstParagraph: "First text",
-        secondParagraph: "Second text",
-        thirdParagraph: "Third text"
+        firstParagraph: "This site was built using React with components for the Google Maps display and the contact form.  The contact form has a single Node endpoint employing Nodemailer.",
+        secondParagraph: "The site was built for an apartment owner in Ann Arbor, Michigan; the majority of his customers are students at the University of Michigan.  The intent was to provide a way to showcase photos of the apartments so that the landlord would not have to manage this task often.",
+        thirdParagraph: "It would be an improvement if I could configure the site so that the landlord could manage the images himself."
        },
     3: {
         title: "Math Dojo",
-        firstParagraph: "First text",
-        secondParagraph: "Second text",
-        thirdParagraph: "Third text"
+        firstParagraph: "This site was built using JQuery Node, and MongoDB.  It is hosted on Heroku and uses mLab to host the database.",
+        secondParagraph: "The intent was to provide a place where my nephew could practice his math.  The themes of animals and martial arts were to appeal to him.",
+        thirdParagraph: "The next step will be to change the selection of the theme; another nephew is a fan of dinosaurs..."
        },
     4: {
         title: "US Language Distribution",
-        firstParagraph: "First text",
-        secondParagraph: "Second text",
-        thirdParagraph: "Third text"
+        firstParagraph: "This site uses JQuery to make an AJAX call to the MapBox API and render the map.  The map is configured with layers that were uploaded and are based on GeoJSON files of census data from 2013.",
+        secondParagraph: "I have always liked languages so it was interesting to see what is spoken in the US.",
+        thirdParagraph: "It would be a good improvement to be able to render the language data more dynamically. "
        },
     5: {
         title: "Simon",
